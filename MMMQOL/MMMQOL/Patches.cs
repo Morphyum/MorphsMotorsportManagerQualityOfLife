b28533fe20e853f8752ea8b88d49aeeca5ddcab4
Morphyum <zion@c-schwabe.de>
2021-01-12 21:54:28 +0100

    *moved Prizefund to Balance mod

--- MMMQOL/MMMQOL/Patches.cs
+++ MMMQOL/MMMQOL/Patches.cs
@@ -13,7 +13,7 @@ namespace MMMQOL
 {
     class Patches
     {
-        static string version = " +MMMQOL-0.14";
+        static string version = " +MMMQOL-0.15";
         [HarmonyPatch(typeof(SetUITextToVersionNumber), "Awake")]
         public static class SetUITextToVersionNumber_Awake_Patch
         {
@@ -221,31 +221,6 @@ namespace MMMQOL
             }
         }
 
-        [HarmonyPatch(typeof(ChampionshipManager), "OnStart")]
-        public static class ChampionshipManager_OnStart_Patch
-        {
-            public static void Postfix(ref ChampionshipManager __instance) {
-                List<Championship> list = __instance.GetChampionshipsForSeries(Championship.Series.GTSeries);
-                foreach (Championship championship in list) {
-                    if (championship.championshipOrderRelative == 0) {
-                        championship.prizeFund += 125000000;
-                    }
-                    else if (championship.championshipOrderRelative == 1) {
-                        championship.prizeFund += 75000000;
-                    }
-                }
-
-                list = __instance.GetChampionshipsForSeries(Championship.Series.EnduranceSeries);
-                foreach (Championship championship in list) {
-                    if (championship.championshipOrderRelative == 0) {
-                        championship.prizeFund += 100000000;
-                    }
-                    else if (championship.championshipOrderRelative == 1) {
-                        championship.prizeFund += 50000000;
-                    }
-                }
-            }
-        }
 
         [HarmonyPatch(typeof(TyreInfoRollover), "Show")]
         public static class TyreInfoRollover_Show_Patch
